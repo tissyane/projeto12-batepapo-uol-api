1a7e61364180a30845776f427b652383e78d3904
Tissyane <tissyane@gmail.com>
2022-09-02 17:01:31 -0300

    Post Request - Route Participant - notfinished

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import joi from "joi";
+// import dotenv from "dotenv"; TODO: Conectar o dotenv
+// dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -7,4 +11,34 @@ app.use(express.json());
 
 const port = "5000";
 
-app.listen(port);
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+
+let db;
+mongoClient.connect().then(() => {
+  db = mongoClient.db("test");
+});
+
+const participantSchema = joi.object({
+  name: joi.string().required(),
+});
+
+/* Participants Routes */
+
+app.post("/participants", (req, res) => {
+  const { name } = req.body;
+
+  const validation = participantSchema.validate({ name });
+
+  if (validation.error) {
+    const loginerror = validation.error.details[0].message;
+    res.status(422).send(loginerror);
+    return;
+  }
+
+  db.collection("participants").insertOne({
+    name: name,
+  });
+  res.sendStatus(201);
+});
+
+app.listen(port, () => console.log("Server listening on 5000"));
